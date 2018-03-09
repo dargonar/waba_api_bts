e813a1c342d2319bfd53e15025e242872f5b0027
= <=>
2018-03-09 19:59:38 +0000

    cambios desde dev para levantar dev-node

--- main_v3.py
+++ main_v3.py
@@ -334,4 +334,4 @@ if __name__ == '__main__':
   def not_found(error):
     return make_response(jsonify({'error': 'not_found'}), 404)
   
-  app.run(host="127.0.0.1", port=int(os.environ.get("PORT",8080)))
+  app.run(host="0.0.0.0", port=int(os.environ.get("PORT",8080)))
